2d4a48287d59281bdeb6d92b56d9e40758aeecdb
Tinameow <dingtingyin@foxmail.com>
2020-12-17 00:13:51 +0800

    just bug

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,26 +1,55 @@
 import numpy as np
+from time import time
+from random import choices, random
+from JobShopScheduling import JSSP
+from copy import deepcopy
 
-
-def generate_rand_seq(m, n):
-    return np.array([np.random.permutation(m) for _ in range(n)])
 
 def init_rand_population(size, m, n):
-    return [generate_rand_seq(m,n) for _ in range(size)]
+    basic = [i for i in range(m) for _ in range(n)]
+    return [np.random.permutation(basic) for _ in range(size)]
 
-def select():
-    pass
 
-def crossover():
-    pass
+def fitness_function_JSSP(chromosome, model):
+    m = model.m_machine
+    n = model.n_job
+    seq = np.zeros((n, m))
+    job_end = np.zeros(m).astype(int)
+    order_end = np.zeros(n).astype(int)
+    for c in chromosome:
+        job = job_end[c]
+        order = order_end[job]
+        seq[job, order] = c
+        job_end[c] += 1
+        order_end[job] += 1
+    return model.get_end_time(seq)
+
+
+def select(population, fitnesses, num):
+    return [tuple(choices(population, weights=fitnesses, k=2)) for _ in num]
+
+def crossover(a, b):
+    child = deepcopy(a)
+    for i in range(len(a)):
+        if random() < 0.5:
+            child[i] = b[i]
+    return child
+
 
 def mutate():
     pass
 
-def genetic_algorithm(f, m_machine, n_job, max_iter):
-    population = init_rand_population(m_machine, n_job)
-    for k in range(max_iter):
-        parents = select(f, population)
-        # children = [crossover(C, population[p[1]], population[p[2]])
-        #             for p in parents]
-        # population = mutate()
 
+def genetic_algorithm(model, max_iter=1000, pop_size=10, select_rate=0.5, crossover_rate=0.8):
+    population = init_rand_population(pop_size, model.m_machine, model.n_job)
+    for k in range(max_iter):
+        fitnesses = np.array([fitness_function_JSSP(p, model) for p in population])
+        parents = select(population, fitnesses, int(select_rate*pop_size))
+        children = [crossover(population[p[1]], population[p[2]])
+                    for p in parents]
+    # population = mutate()
+
+
+if __name__ == '__main__':
+    model = JSSP(2, 3, randopt=True)
+    genetic_algorithm(model)
