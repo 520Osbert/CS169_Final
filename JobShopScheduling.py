7d6080d92990989af14affd01e461568c71ebaa6
Tinameow <dingtingyin@foxmail.com>
2020-12-16 17:37:33 +0800

    Implement JSSP

--- JobShopScheduling.py
+++ JobShopScheduling.py
@@ -4,34 +4,36 @@ class JSSP:
     '''
     m machines, n jobs, each machine need to finish jobs with specific orders
     '''
-    def __init__(self, n, m, cost_matrix):
+    def __init__(self, n, m, Processing_time):
         self.m_machine = m
         self.n_job = n
-        self.Cost = cost_matrix
-        assert self.Cost.shape() == (self.m_machine, self.n_job)
+        self.Processing_time = Processing_time
+        assert self.Processing_time.shape == (self.n_job, self.m_machine)
 
-    def check_valid(self, Seq):
-        '''
-        Check if the sequence is valid: No overlapping
-        '''
-        return True
 
-    def total_processing_time(self, Seq):
+    def get_end_time(self, Seq):
         '''
-        Seq is mxn that specifies the start time to finish jobs for each machine
-        Assert start time i + process time i <= start time i+1
+        Seq is nxm that each column lists the jobs that machine will do in order
         '''
-        assert Seq.shape == (self.m_machine, self.n_job)
-        if not self.check_valid(Seq):
-            return np.inf
-        last_end_job = np.argmax(Seq, axis=1)
-        last_end_time = np.max(Seq, axis=1)
-        end_time = np.max([last_end_time[i] + self.Cost[i, last_end_job[i]] for i in range(self.m_machine)])
-        return end_time
+        assert Seq.shape == (self.n_job, self.m_machine)
+        machine_end_time = np.zeros(self.m_machine)
+        job_end_time = np.zeros(self.n_job)
+        for i in range(self.n_job):
+            for j in range(self.m_machine):
+                job = int(Seq[i, j])
+                print(job, j)
+                end = max(machine_end_time[j], job_end_time[job]) + self.Processing_time[job, j]
+                machine_end_time[j] = end
+                job_end_time[job] = end
+                print(machine_end_time, job_end_time)
+        return np.max(machine_end_time)
 
     def schedule_efficiency(self, Seq):
         '''
         Scheduling efficiency can be defined for a schedule through the ratio
         of total machine idle time to the total processing time
         '''
-        pass
+        end_time = self.get_end_time(Seq)
+        total_processing_time = end_time * self.m_machine
+        idle_time = total_processing_time - np.sum(self.Processing_time)
+        return idle_time / total_processing_time
